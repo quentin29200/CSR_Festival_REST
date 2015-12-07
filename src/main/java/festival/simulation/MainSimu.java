7e48b0d3865d8f17f9785d6dada313f345ed2ab8
quentin29200 <quentin.lgdc@gmail.com>
2015-12-07 17:47:23 +0100

    PATCH ID

--- src/main/java/festival/simulation/MainSimu.java
+++ src/main/java/festival/simulation/MainSimu.java
@@ -2,8 +2,8 @@ package festival.simulation;
 
 public class MainSimu {
 
-	public static void mainSimu(String[] args) {
-		
+	public static void MainSimu(String[] args) {
+
 		Simulation simulation = new Simulation();
 		// Ajout de 5 bus de capacité 40 places
 		simulation.addBuses(2,9);
